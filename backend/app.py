c2f55910bb544d75a729c8d473971fe57da003f0
Virag Hote <viraghote2020@gmail.com>
2025-01-22 10:54:50 +0530

    Initial commit

--- backend/app.py
+++ backend/app.py
@@ -6,20 +6,20 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app) 
 
-# Load the trained model
+
 with open('employee_attrition_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
-# Function to preprocess input data
+
 def preprocess_input(data):
     # Convert input data into a DataFrame
     df = pd.DataFrame(data, index=[0])
 
-    # Map categorical inputs
+    
     df['OverTime'] = df['OverTime'].map({'Yes': 1, 'No': 0})
     df['Gender'] = df['Gender'].map({'Male': 0, 'Female': 1})
 
-    # Perform One-Hot Encoding for categorical features
+     
     df = pd.get_dummies(df,
                         columns=[
                             'BusinessTravel', 'JobRole', 'MaritalStatus',
@@ -27,7 +27,7 @@ def preprocess_input(data):
                         ],
                         drop_first=True)
 
-    # Align the input DataFrame with the model's expected features
+    
     df = df.reindex(columns=model.feature_names_in_, fill_value=0)
 
     return df
@@ -42,7 +42,7 @@ def hello():
 @app.route('/predict', methods=['POST'])
 def predict():
     input_data = request.json
-    processed_input = preprocess_input(input_data)  # Call the preprocess function
+    processed_input = preprocess_input(input_data)  # Call
     prediction = model.predict(processed_input)
 
     return jsonify({'prediction': int(prediction[0])})  # Return prediction as an integer
